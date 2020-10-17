cafaf46fc7762349a70a5d22074743eea4ecec84
crossonym <73015901+crossonym@users.noreply.github.com>
2020-10-17 11:16:08 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Crossonym Pro</h1>
+      <h2>Point to Start you here!</h2>
+    </div>
+  );
+}
